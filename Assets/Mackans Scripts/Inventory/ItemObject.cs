5f7fc80dace667a8b26737e5704b7b6d014f4edb
Lowecleja <lowe.jaja@gmail.com>
2022-12-14 18:40:49 +0100

    Nyckel-ljud till middag

--- Assets/Mackans Scripts/Inventory/ItemObject.cs
+++ Assets/Mackans Scripts/Inventory/ItemObject.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ItemObject : MonoBehaviour
 {
     private InventorySystem inventorySystem;
     private InventorySlotManager inventorySlotManager;
     public InventoryItemData referenceItem;
+    public UnityEvent onTriggerEnter;
 
     private void Start()
-    {
+    { 
+        
         inventorySystem = GameObject.Find("Inventory").GetComponent<InventorySystem>();
         inventorySlotManager = GameObject.Find("InventoryBar").GetComponent<InventorySlotManager>();
+
     }
     
     private void OnTriggerEnter2D(Collider2D col)
     {
+       
         if (col.name.Equals("Human"))
         {
             inventorySystem.Add(referenceItem);
+            onTriggerEnter.Invoke();
             Destroy(gameObject);
+           
         }
     }
 }
