525bca1325126596d465d459b0b4a478f80fe3ab
Carrotbane <marcus60037@gmail.com>
2022-12-19 15:36:45 +0100

    ingen error hos attic nyckeln

--- Assets/Mackans Scripts/Mechanics/PlatformsButton/ObjectsManager.cs
+++ Assets/Mackans Scripts/Mechanics/PlatformsButton/ObjectsManager.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 
 public class ObjectsManager : MonoBehaviour
 {
-    private List<Transform> platformList = new ();
     public bool state = true;
     
-    private void Start()
-    {
-        foreach (Transform child in transform)
-            platformList.Add(child);
-    }
-
     public void Toggle()
     {
         state = !state;
-        foreach (Transform child in platformList)
-        {
-            bool isActive = child.gameObject.activeSelf;
-            child.gameObject.SetActive(!isActive);
-        }
-            
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(!child.gameObject.activeSelf);
     }
 }
